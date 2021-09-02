a949e91be3d48d36bc63f81c5a276610fbd1c68d
Jyoti Machindra Devare <jyotidevare74913@gmail.com>
2021-09-02 20:15:13 +0530

    data types

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-console.log("Hey this is my first piece of js code !!! YAY")
-console.warn("This is warning")
-console.error("This is error")
+//Data types
+//Primitive data type
+let age = 20
+let Name="jyoti"
+let brand 
+let isfalse=false
+console.log(brand)
+
+//Non Primitive data type
+let arr= [1,2,3,4,5]
+arr.push("6")// push, push to last
+arr.unshift(false)//unshift push to start
+console.log(arr)
