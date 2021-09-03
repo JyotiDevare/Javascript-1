37c0150d4caeb1faf6a631a3d32c06d6045eb18b
Jyoti Machindra Devare <jyotidevare74913@gmail.com>
2021-09-03 20:26:27 +0530

    Logical operator

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-let a="2"
-let b=3
-console.log(a-b)
- 
-//comparision operators
+// Logical operator
+const a =2
+const b=3
+console.log(a!=b)
+// AND
+console.log("false && false" ,false && false)
+console.log("false && true" ,false && true)
+console.log("true && false" ,true && false)
+console.log("true && true" ,true && true)
 
-let c=21
-let d= 20
-console.log(c>d)
-console.log(c>=d)
-console.log(c<d)
-console.log(c<=d)
-console.log(c==d)
-console.log(c===d)//first compare value and(&&)the compare type 
+//OR
+console.log("false || false" ,false || false)
+console.log("false || true" ,false || true)
+console.log("true || false" ,true || false)
+console.log("true || true" ,true || true)
