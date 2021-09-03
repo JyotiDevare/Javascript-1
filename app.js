2fb982da799bbdc2dcf8561160b8be75fd981742
Jyoti Machindra Devare <jyotidevare74913@gmail.com>
2021-09-03 20:13:00 +0530

    Comparison operator

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-// Arithmetic Operator
-let age = 19
-age=20
-age=100
-console.log(age)
-const specific_name = "Jyoti"
-console.log(specific_name)
-age ++
-console.log(age)
-const a=2
-const b=5
-console.log(a+b)
+let a="2"
+let b=3
 console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(a**b)
+ 
+//comparision operators
+
+let c=21
+let d= 20
+console.log(c>d)
+console.log(c>=d)
+console.log(c<d)
+console.log(c<=d)
+console.log(c==d)
+console.log(c===d)//first compare value and(&&)the compare type 
